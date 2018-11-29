43396b13e9c4ccabf58a9b988921417435adef18
Matt Dennis <mdennis@visionpointmarketing.com>
2018-11-29 16:16:45 -0500

    begin restructuring of assembly flow

--- js/animationver4.js
+++ js/animationver4.js
@@ -26,14 +26,17 @@ const WINDOW_WIDTH = $(window).width(),
 // Default animation property values.
 const DEFAULT_ANIM_TRIGGER = 'autoplay',
       DEFAULT_ANIM_PROPS = { opacity: "0", ease: Power2.easeOut },
+      DEFAULT_ANIM_EASING = { ease: Power2.easeOut },
       DEFAULT_ANIM_DURATION = 0.5,
       DEFAULT_ANIM_DELAY = 0.2,
-      DEFAULT_ANIM_PLAYBACK = 'forward',
+      //DEFAULT_ANIM_PLAYBACK = 'forward',
 
 // Non-default animation values.
-      ANIM_PROPS_FADE_IN = { opacity: "0", ease: Power2.easeOut },
-      ANIM_PROPS_FADE_IN_SLIDE_UP = { top: "75px", opacity: "0", ease: Power3.easeOut },
-      ANIM_PROPS_FADE_IN_SLIDE_DOWN = { top: "-75px", opacity: "0", ease: Power3.easeOut };
+      ANIM_PROPS_FADE_IN = { opacity: '1', ease: Power2.easeOut },
+      ANIM_PROPS_FADE_OUT = { opacity: '0', ease: Power2.easeOut},
+
+      ANIM_PROPS_SLIDE_UP = { top: "-75px", ease: Power3.easeOut },
+      ANIM_PROPS_SLIDE_DOWN = { top: "75px", ease: Power3.easeOut };
 
       ANIM_TRIGGER_AUTOPLAY = 'autoplay',
       ANIM_TRIGGER_SCROLL_TO = 'scroll-to',
@@ -51,10 +54,10 @@ const DEFAULT_ANIM_TRIGGER = 'autoplay',
       ANIM_DURATION_LONG = 1,
       ANIM_DURATION_XLONG = 2,
       ANIM_DURATION_XXLONG = 3,
-      ANIM_DURATION_XXXLONG = 5,
+      ANIM_DURATION_XXXLONG = 5;
 
-      ANIM_PLAY_FORWARD = 'forward',
-      ANIM_PLAY_REVERSE = 'reverse';
+      //ANIM_PLAY_FORWARD = 'forward',
+      //ANIM_PLAY_REVERSE = 'reverse';
 
 // Variables
 var animTargetList = [];
@@ -91,10 +94,11 @@ function createAnimationObjects(sourceList) {
   sourceList.forEach(function(item) {
     rtn.push({
       animTarget: item,
+      animProps: DEFAULT_ANIM_PROPS,
       animDuration: DEFAULT_ANIM_DURATION,
       animDelay: DEFAULT_ANIM_DELAY,
       animHasRun: false,
-      animPlayback: DEFAULT_ANIM_PLAYBACK
+      //animPlayback: DEFAULT_ANIM_PLAYBACK
     });
   });
   console.log('%c Done!', 'color: green');
@@ -113,23 +117,37 @@ function assignAnimTypes(objList) {
     for (var itemClass of obj.animTarget[0].classList) {
       switch (itemClass) {
         case LIBRARY_BASE_PREFIX + 'fade-in':
-          obj.animProps = ANIM_PROPS_FADE_IN;
-          animTypeFound = !animTypeFound;
+          //obj.animTarget[0].style.opacity = '0';
+          //obj.animProps = Object.assign({}, ANIM_PROPS_FADE_IN);
+          $.extend(true, obj.animProps, ANIM_PROPS_FADE_IN);
+          //animTypeFound = true;
+          //console.log("1");
           break;
-        case LIBRARY_BASE_PREFIX + 'fade-in-slide-up':
-          obj.animProps = ANIM_PROPS_FADE_IN_SLIDE_UP;
-          animTypeFound = !animTypeFound;
+        case LIBRARY_BASE_PREFIX + 'fade-out':
+          //obj.animTarget[0].style.opacity = '1';
+          //obj.animProps = Object.assign({}, ANIM_PROPS_FADE_OUT);
+          $.extend(true, obj.animProps, ANIM_PROPS_FADE_OUT);
+          //animTypeFound = true;
+          //console.log("2");
           break;
-        case LIBRARY_BASE_PREFIX + 'fade-in-slide-down':
-          obj.animProps = ANIM_PROPS_FADE_IN_SLIDE_DOWN;
-          animTypeFound = !animTypeFound;
+        case LIBRARY_BASE_PREFIX + 'slide-up':
+          //obj.animProps = Object.assign({}, ANIM_PROPS_SLIDE_UP);
+          $.extend(true, obj.animProps, ANIM_PROPS_SLIDE_UP);
+          //animTypeFound = true;
+          //console.log("3");
+          break;
+        case LIBRARY_BASE_PREFIX + 'slide-down':
+          ///obj.animProps = Object.assign({}, ANIM_PROPS_SLIDE_DOWN);
+          $.extend(true, obj.animProps, ANIM_PROPS_SLIDE_DOWN);
+          //animTypeFound = true;
+          //console.log("4");
           break;
         default:
           break;
       }
-      if (animTypeFound) return;
+      //if (animTypeFound) return;
     }
-    if (!animTypeFound) obj.animProps = DEFAULT_ANIM_PROPS;
+    //if (!animTypeFound) obj.animProps = DEFAULT_ANIM_PROPS;
   });
   console.log('%c Done!', 'color: green');
 }
@@ -226,29 +244,42 @@ function assignAnimEnhancements(objList) {
 }
 
 // Reset given animation to its appropriate starting point and pause it.
-function resetAnimation(animation, playback, duration) {
+/*function resetAnimation(animation, playback, duration) {
   if (playback === ANIM_PLAY_FORWARD) {
       animation.pause(0);
   } else if (playback === ANIM_PLAY_REVERSE) {
       animation.pause(duration);
   }
+}*/
+function resetAnimation(animation) {
+  animation.restart();
+  animation.pause();
 }
 
 // Look at each object's properties and assemble a new GSAP animation.
-function createAnimations(objList) {
+/*function createAnimations(objList) {
   for (var obj of objList) {
     obj.animation = new TweenLite.from(obj.animTarget, obj.animDuration, obj.animProps);
     resetAnimation(obj.animation, obj.animPlayback, obj.animDuration);
   }
+}*/
+function createAnimations(objList) {
+  for (var obj of objList) {
+    obj.animation = new TweenLite.to(obj.animTarget, obj.animDuration, obj.animProps);
+    resetAnimation(obj.animation);
+  }
 }
 
 // Run the requested animation after any additional delay.
-function runAnimation(anim, delay, playback) {
+/*function runAnimation(anim, delay, playback) {
     if (playback === ANIM_PLAY_FORWARD) {
       TweenLite.delayedCall(delay, function(){anim.play()});
     } else if (playback === ANIM_PLAY_REVERSE) {
       TweenLite.delayedCall(delay, function(){anim.reverse()});
     }
+}*/
+function runAnimation(anim, delay) {
+    TweenLite.delayedCall(delay, function(){anim.play()});
 }
 
 // Set and watch all triggers being used by
@@ -261,7 +292,7 @@ function watchAnimationTriggers(objList) {
       case ANIM_TRIGGER_AUTOPLAY:
         if (!obj.hasRun) {
           obj.hasRun = true;
-          runAnimation(obj.animation, obj.animDelay, obj.animPlayback);
+          runAnimation(obj.animation, obj.animDelay);
         }
         break;
       case ANIM_TRIGGER_SCROLL_TO:
@@ -271,7 +302,7 @@ function watchAnimationTriggers(objList) {
         obj.animTarget.focus(function() {
           if (!obj.hasRun) {
             obj.hasRun = true;
-            runAnimation(obj.animation, obj.animDelay, obj.animPlayback);
+            runAnimation(obj.animation, obj.animDelay);
           }
         });
         break;
@@ -279,13 +310,13 @@ function watchAnimationTriggers(objList) {
         obj.animTarget.hover(function() {
           if (!obj.hasRun) {
             obj.hasRun = true;
-            runAnimation(obj.animation, obj.animDelay, obj.animPlayback);
+            runAnimation(obj.animation, obj.animDelay);
           }
         });
         obj.animTarget.focus(function() {
           if (!obj.hasRun) {
             obj.hasRun = true;
-            runAnimation(obj.animation, obj.animDelay, obj.animPlayback);
+            runAnimation(obj.animation, obj.animDelay);
           }
         });
         break;
@@ -293,7 +324,7 @@ function watchAnimationTriggers(objList) {
         obj.animTarget.click(function() {
           if (!obj.hasRun) {
             obj.hasRun = true;
-            runAnimation(obj.animation, obj.animDelay, obj.animPlayback);
+            runAnimation(obj.animation, obj.animDelay);
           }
         });
         break;
@@ -304,12 +335,31 @@ function watchAnimationTriggers(objList) {
   });
 }
 
+// Gives element relative positioning if not relative or absolute.
+// Without either, element may not animate properly.
+function setElementPositions(objList) {
+  objList.forEach(function(obj) {
+    if (obj.animTarget[0].style.position !== 'relative' || 'absolute') obj.animTarget[0].style.position = 'relative';
+  });
+}
+
 // ========================================================================= //
 // ===== On Load ===== //
 
 $(window).on('load', function(){
   animTargetList = getElementList(LIBRARY_BASE_CLASS);
   animObjectList = createAnimationObjects(animTargetList);
+
+  for (var obj in animObjectList) {
+    setElementPosition(animObjectList);
+    assignAnimType(animObjectList);
+    assignAnimTrigger(animObjectList);
+    assignAnimEnhancements(animObjectList);
+    createAnimation(animObjectList);
+    watchAnimationTrigger(animObjectList);
+  }
+
+  setElementPositions(animObjectList);
   assignAnimTypes(animObjectList);
   assignAnimTriggers(animObjectList);
   assignAnimEnhancements(animObjectList);
